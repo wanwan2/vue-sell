29728d18229f7cb4544c6237ed0e97c6e7f5dac8
Mario <943902842@qq.com>
2017-02-23 19:28:39 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path:'/goods'}">商品</a>
       </div>
@@ -16,7 +16,7 @@
   </div>
 </template>
 <script>
-import header from './components/header/header.vue'
+import header from 'components/header/header.vue'
 
 export default {
   components: {
@@ -25,13 +25,29 @@ export default {
 }
 </script>
 <style>
-.tab{
+  @import "../static/css/mixin.css"; 
+.tab {
   display: flex;
   width: 100%;
   line-height: 40px;
 }
-.tab .tab-item{
+
+.tab .tab-item {
   flex: 1;
   text-align: center;
+}
+
+.tab .tab-item > a{
+  display: block;
+  font-size: 14px;
+  color: rgb(77,85,93);
+}
+
+.tab .tab-item > a.active{
+  color: rgb(240,20,20);
+}
+
+.border-1px:after{
+  border-bottom-color: rgba(7,17,27,.1);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,18 +9,22 @@ Vue.use(VueRouter)
 
 var app = Vue.extend(App)
 
-var router = new VueRouter()
+var router = new VueRouter({
+	linkActiveClass: 'active'
+})
 
 router.map({
-	'/goods': {
-		component: goods
-	},
-	'/ratings': {
-		component: ratings
-	},
-	'/seller': {
-		component: seller
-	}
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
 })
 
 router.start(app, '#app')
+
+router.go('/goods')
