a4b1b66e701cb009436236239e390c35cf97bd5f
Mario <943902842@qq.com>
2017-02-28 19:42:41 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="header">
-    <!-- <div class="header-box">
-      <div class="header-left clearfix">
-        <div class="header-img fl">
-          <img :src="seller.avatar" width="64" height="64" alt="">
-        </div>
-        <div class="header-title">
-          <h3 class="bold"><img src="brand@2x.png" height="18" width="30" alt="">{{seller.name}}</h3>
-          <em class="thin">{{seller.description}} / {{seller.deliveryTime}}分钟送达</em>
-          <span><img src="decrease_1@2x.png" width="14" height="14" alt="">在线支付满28减5，满50减10</span>
-        </div>
-      </div>
-      <a class="header-right thin">5个 <span class="icon icon-keyboard_arrow_right fr"></span></a>
-    </div>
-    <div class="proclamation thin">
-      <p class="over"><img src="bulletin@2x.png" height="12" width="22" alt="">公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告公告</p>
-      <span class="icon icon-keyboard_arrow_right"></span>
-    </div> -->
     <div class="content-wrapper">
       <div class="avatar inline-block"><img width="64" height="64" :src="seller.avatar"></div>
       <div class="content inline-block">
@@ -28,13 +11,20 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div class="supports" v-if="seller.supports">
-          <span class="icon inline-block"></span>
+          <span class="icon inline-block" :class="classMap[seller.supports[0].type]"></span>
           <span class="text thin">{{seller.supports[0].description}}</span>
         </div>
-        <a class="header-right thin">5个 <span class="icon icon-keyboard_arrow_right fr"></span></a>
+      </div>
+      <div v-if="seller.supports" class="support-count thin">
+        <span class="count">{{seller.supports.length}}个</span>
+        <span class="icon-keyboard_arrow_right"></span>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <span class="icon-keyboard_arrow_right"></span>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -43,6 +33,9 @@ export default {
     seller: {
       type: Object
     }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
 }
 </script>
@@ -56,11 +49,15 @@ export default {
   color: #fff;
   font-size: 0;
   .content-wrapper {
+    position: relative;
     padding: 24px 12px 18px 24px;
+    .avatar {
+      border-radius: 2px;
+      overflow: hidden;
+    }
     .content {
       margin-left: 16px;
       .title {
-        display: -webkit-inline-flex;
         display: inline-flex;
         align-items: center;
         margin: 2px 0 8px 0;
@@ -75,7 +72,6 @@ export default {
         }
         .name {
           font-size: 16px;
-          line-height: 1;
         }
       }
       .description,
@@ -87,7 +83,6 @@ export default {
         font-size: 12px;
       }
       .supports {
-        display: -webkit-inline-flex;
         display: inline-flex;
         align-items: center;
         font-size: 10px;
@@ -95,84 +90,54 @@ export default {
           margin-right: 4px;
           width: 14px;
           height: 14px;
-          .bg-images('../../components/header/decrease_1');
           background-size: 14px;
+          &.decrease {
+            .bg-images('../../components/header/decrease_1');
+          }
+          &.discount {
+            .bg-images('../../components/header/discount_1');
+          }
+          &.guarantee {
+            .bg-images('../../components/header/guarantee_1');
+          }
+          &.invoice {
+            .bg-images('../../components/header/invoice_1');
+          }
+          &.special {
+            .bg-images('../../components/header/special_1');
+          }
         }
       }
     }
-  }
-}
-
-.header-box {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  padding: 24px 12px 18px 24px;
-  .header-img {
-    overflow: hidden;
-    background-color: #eee;
-    border-radius: 2px;
-    img {
-      vertical-align: middle;
-    }
-  }
-  .header-title {
-    margin-top: 2px;
-    margin-bottom: 2px;
-    margin-left: 64px;
-    padding-left: 16px;
-    h3 {
-      margin-bottom: 8px;
-      font-size: 16px;
-      img {
-        margin-right: 6px;
-        vertical-align: bottom;
-      }
-    }
-    em,
-    span {
-      display: block;
-      line-height: 12px;
-    }
-    em {
-      margin-bottom: 10px;
-      font-size: 12px;
-      font-style: normal;
-    }
-    span {
-      font-size: 10px;
-      img {
-        margin-right: 4px;
-        vertical-align: bottom;
+    .support-count {
+      display: inline-flex;
+      align-items: center;
+      position: absolute;
+      right: 12px;
+      bottom: 18px;
+      padding: 0 8px;
+      line-height: 24px;
+      border-radius: 14px;
+      background: rgba(0, 0, 0, .2);
+      text-align: center;
+      .count,
+      .icon-keyboard_arrow_right {
+        font-size: 10px;
       }
     }
   }
-  .header-right {
-    padding: 7px 8px;
-    background: rgba(0, 0, 0, .2);
-    color: #fff;
-    border-radius: 12px;
-    font-size: 10px;
-    line-height: 1;
-    .icon {
-      margin-left: 2px;
-      font-size: 10px;
-    }
-  }
-}
-
-.proclamation {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 12px;
-  background: rgba(7, 17, 27, .2);
-  p {
-    font-size: 10px;
+  .bulletin-wrapper {
+    padding: 0 12px;
     line-height: 28px;
-    img {
-      vertical-align: text-top;
-      margin-right: 4px;
+    .over;
+    .bulletin-title{
+      width: 22px;
+      height: 12px;
+      .bg-images('../../components/header/bulletin');
+      background-size: 22px 12px;
+    }
+    .bulletin-text {
+      font-size: 12px;
     }
   }
 }
