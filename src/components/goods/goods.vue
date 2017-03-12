41a227355843a5f8dce9a786ba13cd66b65f763b
Mario <943902842@qq.com>
2017-03-12 23:45:01 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item table">
+        <li v-for="item in goods" class="menu-item table" :class="{'current':currentIndex === $index}">
           <span class="text table-cell">
-    			<span v-show="item.type>0" class="icon inline-block" :class="classMap[item.type]"></span>{{item.name}}
+          <span v-show="item.type>0" class="icon inline-block" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food=item">
-              <div class="icon"><img :src="food.icon"></div>
+            <li v-for="food in item.foods" class="food-item">
+              <div class="icon"><img width="57" height="57" :src="food.icon"></div>
               <div class="content">
-                <h2 class="name">{{food.name}}</h2>
+                <h2 class="name bold">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now bold">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -36,6 +34,7 @@
   </div>
 </template>
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
 const ERR_OK = 0
 export default {
   props: {
@@ -45,7 +44,22 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        } else {
+          return 0
+        }
+      }
     }
   },
   created() {
@@ -54,8 +68,34 @@ export default {
       response = response.body
       if (response.errno === ERR_OK) {
         this.goods = response.data
+        this.$nextTick(() => {
+          this._initScroll()
+          this._calculateHeight()
+        })
       }
     })
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$els.menuWrapper, {})
+      this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+        probeType: 3
+      })
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    _calculateHeight() {
+      let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+        console.log(this.listHeight)
+      }
+    }
   }
 }
 </script>
@@ -78,6 +118,16 @@ export default {
       height: 54px;
       padding: 0 12px;
       line-height: 14px;
+      &.current {
+        position: relative;
+        margin-top: -1px;
+        z-index: 10;
+        background: #fff;
+        font-weight: 700;
+        .text {
+          .border-1px(#fff);
+        }
+      }
       .icon {
         vertical-align: top;
         width: 12px;
@@ -110,6 +160,66 @@ export default {
   }
   .foods-wrapper {
     flex: 1;
+    .title {
+      padding-left: 14px;
+      line-height: 26px;
+      border-left: 2px solid #d9dde1;
+      font-size: 12px;
+      color: rgb(147, 153, 159);
+      background: #f3f5f7;
+    }
+    .food-item {
+      display: flex;
+      margin: 18px;
+      padding-bottom: 18px;
+      &:not(: last-child) {
+        .border-1px(rgba(7, 17, 27, .1));
+      }
+      &:last-child {
+        margin-bottom: 0;
+      }
+      .icon {
+        flex: 0 057px;
+        margin-right: 10px;
+      }
+      .content {
+        flex: 1;
+        .name {
+          margin: 2px 0 8px 0;
+          line-height: 14px;
+          font-size: 14px;
+          color: rgb(7, 17, 27);
+        }
+        .desc {
+          line-height: 12px;
+          margin-bottom: 8px;
+        }
+        .extra {
+          line-height: 10px;
+          .count {
+            margin-right: 12px;
+          }
+        }
+        .desc,
+        .extra {
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+        .price {
+          line-height: 24px;
+          .now {
+            margin-right: 8px;
+            font-size: 14px;
+            color: rgb(240, 20, 20);
+          }
+          .old {
+            text-decoration: line-through;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+        }
+      }
+    }
   }
 }
 </style>
